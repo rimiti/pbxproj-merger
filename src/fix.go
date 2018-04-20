bd04f11c3ecfd265d212a6c5821964cbeb78f03e
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2018-04-20 16:49:55 +0200

    feat(fix.go) read input file and write into output file

--- src/fix.go
+++ src/fix.go
@@ -4,7 +4,7 @@ import (
   "os"
   "fmt"
   "bufio"
-  "io"
+  "log"
 )
 
 func main() {
@@ -12,57 +12,21 @@ func main() {
     fmt.Println("Argument path missing.")
   }
 
-  // open input file
-  fi, err := os.Open(os.Args[1])
+  // Input file
+  inFile, _ := os.Open(os.Args[1])
+  defer inFile.Close()
+  scanner := bufio.NewScanner(inFile)
+  scanner.Split(bufio.ScanLines)
+
+  // Output file
+  outFile, err := os.Create("output.txt")
   if err != nil {
-    panic(err)
+    log.Fatal("Cannot create file", err)
   }
+  defer outFile.Close()
 
-  // close fi on exit and check for its returned error
-  defer func() {
-    if err := fi.Close(); err != nil {
-      panic(err)
-    }
-  }()
-
-  // make a read buffer
-  r := bufio.NewReader(fi)
-
-  // open output file
-  fo, err := os.Create("output.txt")
-  if err != nil {
-    panic(err)
-  }
-  // close fo on exit and check for its returned error
-  defer func() {
-    if err := fo.Close(); err != nil {
-      panic(err)
-    }
-  }()
-  // make a write buffer
-  w := bufio.NewWriter(fo)
-
-  // make a buffer to keep chunks that are read
-  buf := make([]byte, 1024)
-  for {
-    // read a chunk
-    n, err := r.Read(buf)
-    if err != nil && err != io.EOF {
-      panic(err)
-    }
-    if n == 0 {
-      break
-    }
-
-    fmt.Println(fo)
-
-    // write a chunk
-    if _, err := w.Write(buf[:n]); err != nil {
-      panic(err)
-    }
-  }
-
-  if err = w.Flush(); err != nil {
-    panic(err)
+  for scanner.Scan() {
+    fmt.Fprintf(outFile, scanner.Text(), "\n")
+    // fmt.Println("=====>", scanner.Text())
   }
 }
